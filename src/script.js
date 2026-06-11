18559dceef790234eba466d14dca5fa51a6005cb
Nitz19 <136543164+Nitz19@users.noreply.github.com>
2026-06-11 23:53:18 +0800

    update resume and update typing

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 // typing text hero
 const typed = new Typed(".typing-text", {
-  strings: ["Virtual Assistant", "Graphic Designer"],
+  strings: ["General Virtual Assistant", "Social Media Virtual Assistant","GHL Tech"],
   loop: true,
   typeSpeed: 55,
   backSpeed: 25,
